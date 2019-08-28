af53e739d214cf5e5b799a9848a9446d06909563
Rasmus Porsager <rasmus@porsager.com>
2019-08-28 22:53:19 +0200

    Fix queries object lookup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ module.exports = function(queries, handlers) {
     const { name, hql, args } = xs.pop()
     return Promise.resolve(
       handlers[name].apply(handlers[name], xs.concat([
-        typeof queries === 'function' ? queries(hql) : [hql],
+        typeof queries === 'function' ? queries(hql) : queries[hql],
         ...(await Promise.all(args.map(x =>
           x.query
             ? evaluate(x.query)
