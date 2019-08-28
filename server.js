bc792c2c570dd53639d0690d04108e1d18308109
Rasmus Porsager <rasmus@porsager.com>
2019-08-28 17:41:43 +0200

    Allow handlers access to extra context

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 module.exports = function(queries, handlers) {
-  return async function evaluate({
-    name,
-    hql,
-    args
-  }) {
+  return async function evaluate(...xs) {
+    const { name, hql, args } = xs.pop()
     return Promise.resolve(
-      handlers[name](
-        queries[hql],
+      handlers[name].apply(handlers[name], xs.concat([
+        typeof queries === 'function' ? queries(hql) : [hql],
         ...(await Promise.all(args.map(x =>
           x.query
             ? evaluate(x.query)
             : x.value
         )))
-      )
+      ]))
     )
   }
 }
