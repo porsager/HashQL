5e45929b339f7e51fb5a87569d7f38ee8f46197e
Rasmus Porsager <rasmus@porsager.com>
2019-12-12 16:28:04 +0100

    Better error message on missing query

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 module.exports = function(queries, handlers) {
   return async function evaluate({ tag, hash, input }) {
+    const query = typeof queries === 'function' ? queries(hash) : queries[hash]
+    if (!query)
+      throw Object.assign(new Error(hash + ' not found for ' + tag), { code: 'NOT_FOUND', status: 404 })
+
     return Promise.resolve(
       handlers[tag](
-        typeof queries === 'function' ? queries(hash) : queries[hash],
+        query,
         ...(await Promise.all(input.map(x =>
           x.query
             ? evaluate(x.query)
