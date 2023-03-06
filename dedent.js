0501ef503b53744845d4f5f53d7e266cb6dcd231
Rasmus Porsager <rasmus@porsager.com>
2023-03-06 15:46:12 +0100

    Fix dedent

--- dedent.js
+++ dedent.js
@@ -1,15 +1,18 @@
 const indented = (xs) => xs.reduce(
-  (min, x, i) => Math.min(min, (x.match(/^[\t ]*[^\t\n ]/gm) || []).reduce(
-    (min, x, i) => x.length - 1 < min ? x.length - 1 : min,
-    Infinity
-  )),
+  (min, x, i) => Math.min(
+    min,
+    (x.match(/\n[\t ]*[^\t\n ]/g) || []).reduce(
+      (min, x, i) => x.length - 2 <= min ? x.length - 2 : min,
+      Infinity
+    )
+  ),
   Infinity
-)
+) | 0
 
 export default function dedent(xs, ...args) {
   const min = indented(xs)
   xs[0] = xs[0].replace(/^[\r\n\t ]+/g, '')
   xs[xs.length - 1] = xs[xs.length - 1].replace(/[\r\n\t ]+$/g, '')
-  const regexp = new RegExp('^' + ' '.repeat(min), 'gm')
-  return xs.map((x, i) => x.replace(regexp, ''))
+  const regexp = new RegExp('\n' + ' '.repeat(min), 'g')
+  return xs.map((x, i) => x.replace(regexp, '\n'))
 }
